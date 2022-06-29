21510888b52bf8896f5cf0b416f6c83bb57e590d
Suman <iamsuman898@gmail.com>
2022-06-29 19:17:06 +0530

    server.js fixed

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
-    static: "./build"
-})
+const middlewares = jsonServer.defaults();
 
-const port = proces.env.PORT;
+const port = process.env.PORT || 5000;
 server.use(middlewares);
-server.use(jsonServer);
 server.use(router);
-server.listen(port,()=>{console.log("Server running on",port)});
+server.listen(port);
